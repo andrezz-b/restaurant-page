65c356081bf4094ace59502f72d150eb1cb662cb
Andrej Bozic <abozic389@gmail.com>
2022-01-13 17:12:44 +0100

    Tab changing logic.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,46 @@
-import "./styles/main.scss"
-import { createHome } from "./scripts/home"
-import { createMenu } from "./scripts/menu"
+import "./styles/main.scss";
+import { createHome } from "./scripts/home";
+import { createMenu } from "./scripts/menu";
 
-function createSite() {
-    const divContent = document.getElementById("content");
-    divContent.appendChild(createMenu());
-}
+const navController = (function () {
+	const divContent = document.getElementById("content");
+	let currentTab = "home";
 
-createSite();
+	init();
+
+	function init() {
+		divContent.appendChild(createHome());
+		addListeners();
+	}
+
+	function addListeners() {
+		const home = document.querySelector("#home");
+		const menu = document.querySelector("#menu");
+		const about = document.querySelector("#about");
+
+		home.addEventListener("click", changeTab);
+		menu.addEventListener("click", changeTab);
+	}
+
+	function changeTab(e) {
+		const tabToChange = e.target.getAttribute("id");
+		if (currentTab === tabToChange) return;
+
+		currentTab = tabToChange;
+		removeCurrentTab();
+
+		switch (tabToChange) {
+			case "home":
+				divContent.append(createHome());
+				break;
+			case "menu":
+				divContent.append(createMenu());
+				break;
+		}
+	}
+
+	function removeCurrentTab() {
+		const main = divContent.lastElementChild;
+		divContent.removeChild(main);
+	}
+})();
